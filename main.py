5d3b4c0cac6225e1261555ca35ff5de0bbedc005
DiffDude <sasha.funk.02@bk.ru>
2023-07-21 03:42:20 +0500

    Some changes

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pygame as pg
 import random as rd
 
+pg.font.init()
+pg.display.init()
+
 class App:
     def __init__(self):
         self.clock = pg.time.Clock()
-        # Создаем массив слоев, который влияет на: цвет, скорость и размер символов
         # size: [x, speed]
         self.line_arr = dict()
         for sz in all_size:
             s1, s2 = speed_limit
-            self.line_arr[sz] = [[i, rd.randint(s1, s2)/100] for i in range(sz//2, 740+1, sz) if rd.randint(0, 4)]
+            self.line_arr[sz] = [[i, rd.randint(s1, s2)/100] for i in range(sz//2, width+1, sz) if rd.randint(0, 3)]
 
     def run(self):
         # size: x: [func()]
@@ -18,11 +20,9 @@ class App:
             sym_line[sz] = dict()
             for x, speed in self.line_arr[sz]:
                 sym_line[sz][x] = [Symbol_line(sz, -height)]
-                while True:
+                for _ in range(height//sz//9):
                     app_line = sym_line[sz][x]
                     app_line.append(Symbol_line(sz, -sz*2-height-sum([f.length*sz for f in app_line])))
-                    if sum([f.length*sz for f in app_line])+len(app_line)*sz*2 > height:
-                        break
 
         # Главный цикл
         while True:
@@ -30,15 +30,21 @@ class App:
             for sz in range(len(all_size) - 1, -1, -1):
                 size = all_size[sz]
                 for x, speed in self.line_arr[size]:
-                    for f in sym_line[size][x]:
-                        now_draw = f
-                        [now_draw.replace_sym() for _ in ' ' if not rd.randint(0, change_intrv)]
-                        now_draw.move(speed)
+                    for f in range(len(sym_line[size][x])):
+                        now_draw = sym_line[size][x][f]
+                        # Изменить символ в строке
+                        if not rd.randint(0, change_intrv):
+                            now_draw.replace_sym()
+                        # Движение строки вниз или в начало экрана
+                        restart = f+1 if f != len(sym_line[size][x])-1 else 0
+                        now_draw.move(speed, sym_line[size][x][restart].y)
                         now_draw.draw_line(x)
 
             pg.display.flip()
+            [exit() for e in pg.event.get() if e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE]
             [exit() for e in pg.event.get() if e.type == pg.QUIT]
             self.clock.tick(60)
+        pg.quit()
 
 class Symbol_line():
     """Создаем эксемпляр строки, имеет размер length и состоит из списка символов katakana.
@@ -59,12 +65,12 @@ class Symbol_line():
 
         # Список рандомных символов катаканы длиной (length)
         self.font = pg.font.Font('font/ms mincho.ttf', size)
-        self.kana_arr = dict()
         self.kana_arr = [self.font.render(rd.choice(katakana), True, self.colors[k]) for k in range(self.length)]
 
-    def move(self, move):
+    def move(self, move, last):
         # если строка вышла за границу, то строка возвращается обратно
-        self.y += move - ((self.y+self.length*self.size)*(self.y > height))
+        flaq = (self.y > height) and (last > self.size)
+        self.y += move - ((self.y+self.length*self.size)*flaq)
 
     def replace_sym(self):
         # Заменяем рандомный символ из строки
@@ -76,17 +82,22 @@ class Symbol_line():
         for i in range(self.length):
             screen.blit(self.kana_arr[self.length-1-i], (x-self.size//2, self.y+self.size*i))
 
-pg.font.init()
 
-# Начальные параметры. Количество слоев зависит от кол-во элементов all_size
-res = width, height = 740, 380
-color_mtx = (57, 255, 20) #85,85,255
-speed_limit = (100, 300)
-change_intrv = 20
-all_size = [40, 30, 20, 10]
+# START VALUE
+katakana = [chr(i) for i in range(0xFF66,0xFF9E)]  # '1234567890'.strip()
+all_size = [60, 30, 20, 10]  # Размеры символов и кол-во слоев
+color_mtx = (255, 7, 58)     # 57, 255, 20 | 85, 85, 255 | 254, 89, 194
+speed_limit = (200, 400)     # Минимальная и максимальная скорость
+change_intrv = 49            # Шанс изменения символа в строке
 
-screen = pg.display.set_mode(res, pg.SCALED)
-katakana = [chr(i) for i in range(0xFF66,0xFF9E)]
+# SCALED SCREEN
+# res = width, height = 740, 380
+# screen = pg.display.set_mode(res, pg.SCALED)
+
+# FULLSCREEN
+display = pg.display.Info()
+res = width, height = display.current_w, display.current_h
+screen = pg.display.set_mode(res, pg.FULLSCREEN)
 
 if __name__ == '__main__':
     app = App()
